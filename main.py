49c9f95d583d69b3ff4f42dd677a396be5d40c71
madblock6 <ryanbatista@mail.weber.edu>
2023-02-13 14:51:03 -0700

    Intro to game

--- main.py
+++ main.py
@@ -6,7 +6,13 @@ def main():
 	while home != "quit":
 		home = input(f'Type "quit" to quit the game\nType "start" to start the game\nType "leaderboard" to see the leaderboard\n')
 		if home == "start":
-			print(f"Welcome to the luna")
+			print(f"\nWelcome to the luna\n\n")
+			print(f"Your adventure starts in a prison cell you apear to be in a spaceship prison.\nYou see other cells with nobody in them and a gaurd asleep, holding a key.\nIt apears that the cell you are in is unlocked.")
+			choice1 = input(f'Type "leave" to leave the cell and explore the ship, type "stay" to stay in your cell.\n')
+			if choice1 == "leave":
+				print(f'\nyou leave your cell and are standing in a hallway with a door at the end of it and multiple other cells.\nYou see the guard with a key, type "steal" to steal the key')
+			elif choice1 == "stay":
+				print(f"You stay in your prison cell")
 			break
 		if home == "leaderboard":
 			print(f"\tLeaderboard:\nTop score:{score}\nLowest score:{scorelow}\n")
